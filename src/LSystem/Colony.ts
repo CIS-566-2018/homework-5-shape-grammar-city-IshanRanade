0391c1867b5c5959d2aa3711e4043ac80694e0b4
Ishan Ranade <ranadeishan@gmail.com>
2018-02-27 21:01:18 -0500

    make a decent looking colony

--- src/LSystem/Colony.ts
+++ src/LSystem/Colony.ts
@@ -1,4 +1,4 @@
-import { vec3, vec4 } from "gl-matrix";
+import { vec3, vec4, quat } from "gl-matrix";
 import PropertyHolder from "./PropertyHolder";
 import Unit from "./Unit";
 
@@ -39,14 +39,17 @@ class Colony {
     while(queue.length > 0) {
       let curData = queue.pop();
 
-      if(curData.level < 4) {
+      let maxLevel: number = 3;
+      if(curData.level < maxLevel) {
         let nextColonyAim = vec3.fromValues(1,0,0);
         vec3.scale(nextColonyAim, nextColonyAim, curData.distance);
         vec3.add(nextColonyAim, nextColonyAim, curData.center);
 
-        for(let i: number = 0; i < 5; ++i) {
+        for(let i: number = 0; i < 15; ++i) {
           let nextColonyPosition: vec3 = vec3.create();
-          vec3.rotateY(nextColonyPosition, nextColonyAim, vec3.fromValues(0,0,0), this.seed() * 360);
+
+          let degrees = this.seed() * 360;
+          vec3.rotateY(nextColonyPosition, nextColonyAim, curData.center, degrees * Math.PI / 180);
 
           var colonyCoord = {
             coordinates: [nextColonyPosition[0], nextColonyPosition[1], nextColonyPosition[2]],
@@ -55,16 +58,32 @@ class Colony {
 
           var nearest = nearestTrees.nearest(colonyCoord, 1);
           if((nearest.length > 0 && nearest[0][1] < curData.radius) || 
-              (vec3.distance(nextColonyPosition, this.center)) + curData.radius > this.radius * 4) {
+              (vec3.distance(nextColonyPosition, this.center)) + curData.radius > this.radius * 3) {
             continue;
           }
 
           nearestTrees.insert(colonyCoord);
           let unit: Unit = new Unit(this.geometry, this.seed, nextColonyPosition, this.up, curData.radius);
 
+          // Add a road
+          let q: quat = quat.create();
+          quat.rotateY(q, q, degrees * Math.PI / 180);
+          quat.normalize(q, q);
+          let translation: vec3 = vec3.create();
+          vec3.add(translation, curData.center, nextColonyPosition);
+          vec3.scale(translation, translation, 0.5);
+          this.geometry['road'].add(vec4.fromValues(translation[0], translation[1], translation[2], 1),
+                                    vec4.fromValues(q[0], q[1], q[2], q[3]), 
+                                    vec4.fromValues(vec3.distance(nextColonyPosition, curData.center) * 0.15,0.4,0.3 * (maxLevel - curData.level),1));
+
+          nearestTrees.insert({
+            coordinates: [translation[0], translation[1], translation[2]],
+            radius: curData.radius/2.0
+          });
+
           queue.push({center: vec3.fromValues(nextColonyPosition[0], nextColonyPosition[1], nextColonyPosition[2]),
-                      radius: curData.radius * 0.8,
-                      distance: curData.distance * 0.5,
+                      radius: curData.radius * 0.7,
+                      distance: curData.distance * 0.6,
                       level: curData.level + 1});
         }
       }
